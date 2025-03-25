d15f049a75e9357787a44e1f3fb90d5af704cb34
Luis Chinchilla <luis.marroquin@galileo.edu>
2025-03-25 03:04:33 +0000

    proyecto php

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fundamentos_Actividad4_PhP
---
+++ actividad3.php
@@ -0,0 +1,7 @@
+<?php
+    $variable_nombre = "Luis Adolfo Chinchilla";
+    echo "Hola Mundo, soy " . $variable_nombre;
+?>
+<br/>
+<img src ="/Actividad3_SeccionA/proyecto_php/perroIA.jpeg">
+
